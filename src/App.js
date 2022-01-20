5371c3aa97102db56f7b2648eb29a815ba7bd938
Nikothaboss <57292271+Nikothaboss@users.noreply.github.com>
2022-01-20 23:29:52 +0100

    style: style testing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { AppWrapper, colors } from "./app.styled";
 import { useColorModeValue, useColorMode } from "@chakra-ui/color-mode";
+import { Flex } from "@chakra-ui/react";
 
 
 
@@ -8,7 +9,7 @@ function App() {
   const bg = useColorModeValue(colors.lightModeBg, colors.darkModeBg);
   return (
     <AppWrapper bg={bg} onClick={toggleColorMode} minH="100vh" maxH="100%"  className="App">
-      <button>{colorMode}</button>
+      <Flex fontSize="10rem" justifyContent="center">{colorMode}</Flex>
     </AppWrapper>
   );
 }
